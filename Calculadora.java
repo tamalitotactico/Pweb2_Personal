03198999a2d552d1f62fa6b76a2b62317b7be90c
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-04-28 21:18:19 -0500

    Dividí todas las operaciones en métodos diferentes, se puede hacer desde el main, pero es más ordenado así.

--- Calculadora.java
+++ Calculadora.java
@@ -2,4 +2,24 @@ public class Calculadora {
     public static void main(String[] args) {
 
     }
+
+    public static void add() {
+
+    }
+
+    public static void sub() {
+
+    }
+
+    public static void mul() {
+
+    }
+
+    public static void div() {
+
+    }
+
+    public static void mod() {
+
+    }
 }
