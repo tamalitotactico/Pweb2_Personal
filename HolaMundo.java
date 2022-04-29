ed1b75b1e23266f9cc1b57403f3cadf5688f3c06
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-04-28 21:02:52 -0500

    Hice unas modificaciones en el archivo HolaMundo.java, agregué un comentario y la impresión de una sentencia extra

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,9 @@
 
 public class HolaMundo {
     public static void main(String[] args) {
+
+        // Aquí imprimo el hola mundo
         System.out.println("Hola Mundo!");
+        System.out.println("Soy César! :D");
     }
 }
