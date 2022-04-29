af97f4e8ae5c39db836711d61d7a340a215231ed
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-04-28 21:27:15 -0500

    Creé un scanner y agregué las variables donde se guardarán los datos. Además agregué lineas de código que imprimen texto de guía para el usuario.

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Calculadora {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el primer número a operar");
+        int num1 = sc.nextInt();
+        System.out.println("Ingrese el segundo número a operar");
+        int num2 = sc.nextInt();
+        System.out.println("Ingrese la operación a realizar");
+        String operation = sc.nextLine();
     }
 
     public static void add() {
