216ee86caf2d7a3671a92a97ac4a5b1a07f50bb3
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-04-28 21:50:34 -0500

    Se insertó todo el código de main (menos la instancia del scanner) a un bucle. Este bucle for hará que la calculadora se pueda ejecutar repetidas veces.

--- Calculadora.java
+++ Calculadora.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el primer número a operar");
-        int num1 = sc.nextInt();
-        System.out.println("Ingrese el segundo número a operar");
-        int num2 = sc.nextInt();
-        System.out.println("Ingrese la operación a realizar");
-        String operation = sc.next();
-        operate(num1, num2, operation);
+
+        for (;;) {
+            System.out.println("Ingrese el primer número a operar");
+            int num1 = sc.nextInt();
+            System.out.println("Ingrese el segundo número a operar");
+            int num2 = sc.nextInt();
+            System.out.println("Ingrese la operación a realizar");
+            String operation = sc.next();
+            operate(num1, num2, operation);
+            System.out.println("\n-----------------------------\n");
+        }
     }
 
     public static void operate(int num1, int num2, String operation) {
